a15849a6898cd731cb68efe05de563ca4467ac6d
JuricLeo <ljurickc@gmail.com>
2024-03-04 19:26:14 +0100

    testimonials created

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Contact from "@/components/layout/home-page/contact";
 import CustomerStatements from "@/components/layout/home-page/image-carousel";
 import FinishedJobCounter from "@/components/layout/home-page/finished-jobs-counter";
+import Testimonials from "@/components/layout/home-page/testimonials";
 
 export default function Home() {
   return (
     <main className="">
+      <Testimonials />
       <CustomerStatements />
       <FinishedJobCounter />
       <Contact />

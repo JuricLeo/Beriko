0a00d2f139f8e57b398032a8ec1dd95f395cc75a
LukaD45 <luka.dusko44@gmail.com>
2024-04-04 13:33:44 +0200

    About page polishing

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,11 +1,9 @@
-import { StickyScrollRevealDemo } from "@/components/layout/about-page/sticky-scroll-content";
 import { Timeline } from "@/components/layout/about-page/tracing-beam-content";
 
 export default function About() {
   return (
     <div>
       <Timeline />
-      <StickyScrollRevealDemo />
     </div>
   );
 }
--- components/layout/main-layout/navbar.tsx
+++ components/layout/main-layout/navbar.tsx
@@ -45,11 +45,11 @@ export default function Navbar() {
           <Image alt="Beriko" src="/logo.png" width={200} height={200} />
           <div className="gap-x-10 mr-10 hidden lg:flex">
             <Link href="/">Naslovna</Link>
-          </p>
-          <p>
-            <Link href="/about">O nama</Link>
-          </p>
-          <p>
+
+            <p>
+              <Link href="/about">O nama</Link>
+            </p>
+
             <Link href="#">Recenzije</Link>
             <DropdownMenu>
               <DropdownMenuTrigger className="flex items-center">

8f7f357b9175d34cdf4644dd6c1326e678a335bc
LukaD45 <luka.dusko44@gmail.com>
2024-04-10 09:12:56 +0200

    Contact page

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -1,5 +1,18 @@
+import ContactForm from "@/components/layout/contact-page/contact-form";
+import ContactInfo from "@/components/layout/contact-page/contact-info";
+import ContactHeroSection from "@/components/layout/contact-page/hero-section";
+import FAQSection from "@/components/layout/home-page/faqs-section";
+import Map from "@/components/layout/home-page/map";
+
 export default function Contact() {
   return (
-    <main>page</main>
-  )
+    <main>
+      <ContactHeroSection />
+      <ContactForm />
+      <ContactInfo />
+
+      <Map />
+      <FAQSection />
+    </main>
+  );
 }

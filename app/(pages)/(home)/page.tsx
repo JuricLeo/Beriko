3dcb733b3b222b67d91dec28399d7c569b51f529
JuricLeo <ljurickc@gmail.com>
2024-04-11 12:32:13 +0200

    leaflet map

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -2,7 +2,6 @@ import Contact from "@/components/layout/home-page/contact";
 import ImageSlider from "@/components/layout/home-page/image-carousel";
 import FinishedJobCounter from "@/components/layout/home-page/finished-jobs-counter";
 import Testimonials from "@/components/layout/home-page/testimonials";
-
 import Hero from "@/components/layout/home-page/hero";
 import FeatureStrenght from "@/components/layout/home-page/feature-strenght";
 import QualitySection from "@/components/layout/home-page/quality-section";
@@ -10,6 +9,13 @@ import FeatureGarages from "@/components/layout/home-page/feature-garages";
 import ServicesSection from "@/components/layout/home-page/services-section";
 import FAQSection from "@/components/layout/home-page/faqs-section";
 import Map from "@/components/layout/home-page/map";
+
+import dynamic from "next/dynamic";
+
+const LazyMap = dynamic(() => import("@/components/layout/home-page/leaflet-map"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Home() {
   return (
@@ -21,6 +27,7 @@ export default function Home() {
       <FeatureGarages />
       <FinishedJobCounter />
       <ServicesSection />
+      <LazyMap />
       <Testimonials />
       <FAQSection />
       <Map />

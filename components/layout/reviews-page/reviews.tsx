a76b837265b62414d4ce86c5bc5e6e290a6063e5
JuricLeo <ljurickc@gmail.com>
2024-04-11 20:15:37 +0200

    deployment fixes

--- components/layout/reviews-page/reviews.tsx
+++ components/layout/reviews-page/reviews.tsx
@@ -10,47 +10,49 @@ export default function Reviews() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center mt-10">
         <div className="shadow-xl rounded-xl h-fit">
           <p className="bg-[color:var(--light)] p-4 rounded-xl">
-            "Beriko je nadmašio naša očekivanja svojim proizvodima vrhunske
-            kvalitete i besprijekornom uslugom Beriko je nadmašio naša očekivanja svojim proizvodima vrhunske
-            kvalitete i besprijekornom uslugom Beriko je nadmašio naša očekivanja svojim proizvodima vrhunske
-            kvalitete i besprijekornom uslugom".
+            &quot;Beriko je nadmašio naša očekivanja svojim proizvodima vrhunske
+            kvalitete i besprijekornom uslugom Beriko je nadmašio naša
+            očekivanja svojim proizvodima vrhunske kvalitete i besprijekornom
+            uslugom Beriko je nadmašio naša očekivanja svojim proizvodima
+            vrhunske kvalitete i besprijekornom uslugom.&quot;
           </p>
           <h3 className="p-3 mr-4 text-end"> - Ana Petrović</h3>
         </div>
         <div className="shadow-xl rounded-xl h-fit">
           <p className="bg-[color:var(--light)] p-4 rounded-xl">
-            "Vjerujem Beriku zbog njihove dosljedne isporuke visokokvalitetnih
-            rješenja po konkurentnim cijenama."
+            &quot;Vjerujem Beriku zbog njihove dosljedne isporuke
+            visokokvalitetnih rješenja po konkurentnim cijenama.&quot;
           </p>
           <h3 className="p-3 mr-4 text-end"> - Luka Marko</h3>
         </div>
         <div className="shadow-xl rounded-xl h-fit">
           <p className="bg-[color:var(--light)] p-4 rounded-xl">
-            "Beriko nudi nevjerojatnu vrijednost za novac, s proizvodima koji
-            nadmašuju očekivanja."
+            &quot;Beriko nudi nevjerojatnu vrijednost za novac, s proizvodima
+            koji nadmašuju očekivanja.&quot;
           </p>
           <h3 className="p-3 mr-4 text-end"> - Ivan Matej</h3>
         </div>
         <div className="shadow-xl rounded-xl h-fit">
           <p className="bg-[color:var(--light)] p-4 rounded-xl">
-            "Beriko je nadmašio naša očekivanja svojim proizvodima vrhunske
-            kvalitete i besprijekornom uslugom Beriko je nadmašio naša očekivanja svojim proizvodima vrhunske
-            kvalitete i besprijekornom uslugom Beriko je nadmašio naša očekivanja svojim proizvodima vrhunske
-            kvalitete i besprijekornom uslugom".
+            &quot;Beriko je nadmašio naša očekivanja svojim proizvodima vrhunske
+            kvalitete i besprijekornom uslugom Beriko je nadmašio naša
+            očekivanja svojim proizvodima vrhunske kvalitete i besprijekornom
+            uslugom Beriko je nadmašio naša očekivanja svojim proizvodima
+            vrhunske kvalitete i besprijekornom uslugom.&quot;
           </p>
           <h3 className="p-3 mr-4 text-end"> - Ana Petrović</h3>
         </div>
         <div className="shadow-xl rounded-xl h-fit">
           <p className="bg-[color:var(--light)] p-4 rounded-xl">
-            "Vjerujem Beriku zbog njihove dosljedne isporuke visokokvalitetnih
-            rješenja po konkurentnim cijenama."
+            &quot;Vjerujem Beriku zbog njihove dosljedne isporuke
+            visokokvalitetnih rješenja po konkurentnim cijenama.&quot;
           </p>
           <h3 className="p-3 mr-4 text-end"> - Luka Marko</h3>
         </div>
         <div className="shadow-xl rounded-xl h-fit">
           <p className="bg-[color:var(--light)] p-4 rounded-xl">
-            "Beriko nudi nevjerojatnu vrijednost za novac, s proizvodima koji
-            nadmašuju očekivanja."
+            &quot;Beriko nudi nevjerojatnu vrijednost za novac, s proizvodima
+            koji nadmašuju očekivanja.&quot;
           </p>
           <h3 className="p-3 mr-4 text-end"> - Ivan Matej</h3>
         </div>
